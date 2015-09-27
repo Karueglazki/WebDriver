b6c7566bd4393e0a4f580034ef1e5cbf8ec2f173
karueglazki <karueglazki@gmail.com>
2015-09-27 11:14:08 +0300

    system log

--- src/webdrivertest/TestBase.java
+++ src/webdrivertest/TestBase.java
@@ -3,11 +3,19 @@ package webdrivertest;
 import static org.junit.Assert.fail;
 
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.logging.Logs;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -17,9 +25,27 @@ public class TestBase {
 
 	@Before
 	public void setUp() throws Exception {
-	    driver = new FirefoxDriver();
-	    baseUrl = "https://www.google.com.ua/";
+		LoggingPreferences logs = new LoggingPreferences();
+//		logs.enable(LogType.BROWSER, Level.OFF);
+//		logs.enable(LogType.CLIENT, Level.SEVERE);
+//		logs.enable(LogType.DRIVER, Level.WARNING);
+//		logs.enable(LogType.PERFORMANCE, Level.INFO);
+		logs.enable(LogType.SERVER, Level.ALL);
+
+		DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+		desiredCapabilities.setCapability(CapabilityType.LOGGING_PREFS, logs);
+
+//		WebDriver driver = new FirefoxDriver(desiredCapabilities);
+	    driver = new FirefoxDriver(desiredCapabilities);
+	    baseUrl = "http://presentain.com/";
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    
+	    Logs logs2 = driver.manage().logs();
+	    LogEntries logEntries = logs2.get(LogType.DRIVER);
+
+	    for (LogEntry logEntry : logEntries) {
+	        System.out.println(logEntry.getMessage());
+	    }
 	  }
 
 	@After
--- src/webdrivertest/WebDriver.java
+++ src/webdrivertest/WebDriver.java
@@ -3,13 +3,17 @@ package webdrivertest;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 
 public class WebDriver extends TestBase {
   @Test
   public void testGoogleJunit4WDriver() throws Exception {
-	  driver.get("https://www.google.com.ua/");
-		driver.findElement(By.id("lst-ib")).sendKeys("обучение собак");
-		driver.findElement(By.name("btnG")).click();
+	driver.get("https://www.google.com.ua/");
+	driver.findElement(By.id("lst-ib")).sendKeys("обучение собак");
+	driver.findElement(By.name("btnG")).click();
+//	WebElement weblink= driver.findElement(By.xpath("//*[@id='rso']//a"));
+//	Actions builder = new Actions(driver);
+//	builder.contextClick(weblink).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
 
 
   }
