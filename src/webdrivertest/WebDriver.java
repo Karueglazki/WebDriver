7ee700feac46f35f6d52f62ba88ac946880652da
karueglazki <karueglazki@gmail.com>
2015-09-27 11:16:54 +0300

    open new tab and close

--- src/webdrivertest/WebDriver.java
+++ src/webdrivertest/WebDriver.java
@@ -6,16 +6,30 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 
 public class WebDriver extends TestBase {
+
   @Test
-  public void testGoogleJunit4WDriver() throws Exception {
-	driver.get("https://www.google.com.ua/");
-	driver.findElement(By.id("lst-ib")).sendKeys("обучение собак");
-	driver.findElement(By.name("btnG")).click();
-//	WebElement weblink= driver.findElement(By.xpath("//*[@id='rso']//a"));
-//	Actions builder = new Actions(driver);
-//	builder.contextClick(weblink).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
+  public void testWindowPopup()
+  {
+	  driver.get("http://presentain.com/");
+      //Save the WindowHandle of Parent Browser Window
+      String parentWindowId = driver.getWindowHandle();
 
+      //Clicking Help Button will open Help Page in a new Popup Browser Window
+      WebElement helpButton = driver.findElement(By.cssSelector(".social li a i.fa-facebook"));
+      helpButton.click();
+      System.out.println(driver.getTitle());
 
+      for (String winHandle : driver.getWindowHandles()) {
+    	    driver.switchTo().window(winHandle); // switch focus of WebDriver to the next found window handle (that's your newly opened window)
+    	}
+      
+      //Close the Help Popup Window
+      driver.close();
+
+      //Move back to the Parent Browser Window
+      driver.switchTo().window(parentWindowId);
+
+      //Verify the driver context is in Parent Browser Window
+      assertTrue(driver.getTitle().equals("Present. Engage. Grow. - Presentain"));
   }
-
 }
